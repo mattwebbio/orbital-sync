4892cbdde3ef41e3a81d5cc0c69ccbe7922c2a99
Matt Webb <hello@mattwebb.io>
2022-08-26 07:17:36 -0700

    Add eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  rules: {
+    'no-constant-condition': ['error', { checkLoops: false }]
+  }
+};
