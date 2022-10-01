21737a96fee2de8c0db50771f3bfe12cee637144
Matt Webb <hello@mattwebb.io>
2022-09-30 18:43:23 -0700

    Stringify non-string logging

--- src/log.ts
+++ src/log.ts
@@ -3,15 +3,20 @@ import { Config } from './config.js';
 
 export class Log {
   static info(message: unknown) {
-    console.log(`${this.timestamp}: ${message}`);
+    console.log(`${this.timestamp}: ${this.stringify(message)}`);
   }
 
   static verbose(message: unknown) {
-    if (Config.verboseMode && message) this.info(message);
+    if (Config.verboseMode && message) this.info(this.stringify(message));
   }
 
   static error(message: unknown) {
-    console.error(`${this.timestamp}: ${chalk.red(message)}`);
+    console.error(`${this.timestamp}: ${chalk.red(this.stringify(message))}`);
+  }
+
+  private static stringify(message: unknown): string {
+    if (typeof message === 'string') return message;
+    else return JSON.stringify(message);
   }
 
   private static get timestamp(): string {
