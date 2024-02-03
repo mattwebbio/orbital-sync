665740e843427746034d79bfb87274ff5a947399
Matt Webb <matthew.webb@appfolio.com>
2024-02-03 13:55:18 -0800

    test: remove deprecated globals
    Was getting the following error when running tests:
    ```
    ts-jest[ts-jest-transformer] (WARN) Define `ts-jest` config under `globals` is deprecated. Please do
    transform: {
        <transform_regex>: ['ts-jest', { /* ts-jest config goes here in Jest */ }],
    },
    ```
    
    I think this would probably look like:
    ```
      transform: {
        '^.+\\.ts$': ['ts-jest', { useESM: true }]
      },
    ```
    
    However, upon removing it completely, no tests seem to fail so I'd rather remove than have dead code.

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,11 +2,6 @@
 module.exports = {
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
-  globals: {
-    'ts-jest': {
-      useESM: true
-    }
-  },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1'
   },
